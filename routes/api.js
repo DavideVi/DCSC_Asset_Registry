5852fb1ed6d4eb32a9226caee367b6cc74b68e61
teodor.i.zangur <teo@zan.com>
2017-03-12 01:27:30 +0000

    Implemented asset get, asset list.

--- routes/api.js
+++ routes/api.js
@@ -42,13 +42,42 @@ router.post('/asset/add', function(req, res, next) {
 });
 
 // Returns asset information
-router.get('/asset/get', function(req, res, next) {
-  return res.json({"status": "ok"});
+router.get('/asset/get/:asset_id', function(req, res, next) {
+
+  if (req.params.asset_id === undefined) {
+    return res.status(400).json({"error": asset_id + " has not been set or is invalid"});
+  }
+
+  var mongo = require('mongodb');
+  var asset_id = new mongo.ObjectID(req.params.asset_id);
+
+  var collection = req.db.get('assets');
+  collection.find({
+    _id: asset_id
+  }, function (err, doc) {
+
+      if (err) {
+        return res.status(500).json({"status": "error", "message": "Invalid ID"});
+      }
+      else {
+        return res.json(doc);
+      }
+  })
 });
 
 // Returns a list of assets
 router.get('/asset/list', function(req, res, next) {
 
+  var collection = req.db.get('assets');
+  collection.find({}, '-asset_purpose -author_ids -technologies -stability -scm_link -wiki_link', function (err, doc) {
+
+      if (err) {
+        return res.status(500).json({"status": "error", "message": err});
+      }
+      else {
+        return res.json(doc);
+      }
+  })
 });
 
 // Updates information regarding an asset
