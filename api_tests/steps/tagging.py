95c6d0b81be0452c0440db9fc489b3f04a249156
Davide <davide@davidevitelaru.com>
2017-03-24 01:29:58 +0000

    Deleted cucumber steps, added .pyc to .gitignore

--- api_tests/steps/tagging.py
+++
@@ -1,8 +0,0 @@
-from behave import *
-import requests, os
-
-ENDPOINT = os.environ['ASSET_REG_ENDPOINT']
-
-@then('then the relevant tags should be extracted')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then then the relevant tags should be extracted')
